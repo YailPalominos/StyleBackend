63615ee317f2fb3e86c05618dfda89b43da40f85
YailPalominos <yail.palominos@gmail.com>
2023-04-01 00:46:32 -0600

    Initial commit

---
+++ StyleBackend/Controllers/UsuarioController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StyleFrontend.Modelos;
+
+namespace StyleBackend.Controllers
+{
+
+    public class UsuarioController : Controller
+    {
+
+        
+        [HttpGet("{nId:int}")]
+        public async Task<IActionResult> Get(int nId)
+        {
+            try
+            {
+                var oUsuario=new Usuario();
+
+                return Ok(oUsuario);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+
+    }
+}
---
+++ StyleBackend/Models/Usuario.cs
@@ -0,0 +1,34 @@
+﻿namespace StyleFrontend.Modelos
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public string NombreUsuario { get; set; }
+        public string Contraseña { get; set; }
+        public string Pais { get; set; }
+        public string Estado { get; set; }
+        public string FechaNacimiento { get; set; }
+        public string Amigos { get; set; }
+        public string CorreoElectronico { get; set; }
+        public string FechaRegistro { get; set; }
+
+        public Usuario()
+        {
+            Id=0;
+            Nombres = "";
+            Apellidos = "";
+            NombreUsuario = "";
+            Contraseña = "";
+            Pais = "";
+            Estado = "";
+            FechaNacimiento = "";
+            Amigos = "";
+            CorreoElectronico = "";
+            FechaRegistro = "";
+        }
+
+    }
+   
+}
